76cfb427d39e711ee88b31bb8e7502048d0712d8
luccasscds <lucassilvacds7@gmail.com>
2021-10-06 23:30:29 -0300

    modify main.js perfomerce on project and add mascara string on princes

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,5 +1,5 @@
 import toggleModal from "./modal.js";
-import { get, slice, storage } from "./salary.js"
+import { slice, storage, formatMoney } from "./salary.js"
 
 const buttom = document.querySelector('.edit-salary');
 buttom.addEventListener('click', toggleModal);
@@ -7,36 +7,29 @@ buttom.addEventListener('click', toggleModal);
 const closeButtom = document.querySelector('.close');
 closeButtom.addEventListener('click', toggleModal);
 
+function setDomValues(salary, requiredAccount, investment, freePass, books){
+    document.querySelector('#salary').value = salary;    
+    document.querySelector('#textSalary').innerText = `R$ ${formatMoney(Number(salary))}`;
+    document.querySelector('#requiredAccount').innerText = `R$ ${formatMoney(Number(requiredAccount))}`;
+    document.querySelector('#investment').innerText = `R$ ${formatMoney(Number(investment))}`;
+    document.querySelector('#freePass').innerText = `R$ ${formatMoney(Number(freePass))}`;
+    document.querySelector('#books').innerText = `R$ ${formatMoney(Number(books))}`;
+};
+
 const input = document.querySelector('#salary');
 input.addEventListener('keypress', (event) => {
     const enter = event.keyCode;
     if(enter === 13) {
         const { salary, requiredAccount, investment, freePass, books } = slice();
-        const salaryText = document.querySelector('#textSalary');
-        const requiredAccountText = document.querySelector('#requiredAccount');
-        const investmentText = document.querySelector('#investment');
-        const freePassText = document.querySelector('#freePass');
-        const booksText = document.querySelector('#books');
 
-        salaryText.innerText = `R$ ${Number(salary).toFixed('2').replace('.',',')}`;
-        requiredAccountText.innerText = `R$ ${Number(requiredAccount).toFixed('2').replace('.',',')}`;
-        investmentText.innerText = `R$ ${Number(investment).toFixed('2').replace('.',',')}`;
-        freePassText.innerText = `R$ ${Number(freePass).toFixed('2').replace('.',',')}`;
-        booksText.innerText = `R$ ${Number(books).toFixed('2').replace('.',',')}`;
+        setDomValues(salary,requiredAccount,investment,freePass,books);
+
         storage(salary);
         toggleModal();
     };
 });
 
-function setDom(){
-    const salary = localStorage.getItem('currentSalary');
-    document.querySelector('#salary').value = salary;
-    const { requiredAccount, investment, freePass, books } = slice();
-    
-    document.querySelector('#textSalary').innerText = `R$ ${Number(salary).toFixed('2').replace('.',',')}`;
-    document.querySelector('#requiredAccount').innerText = `R$ ${Number(requiredAccount).toFixed('2').replace('.',',')}`;
-    document.querySelector('#investment').innerText = `R$ ${Number(investment).toFixed('2').replace('.',',')}`;
-    document.querySelector('#freePass').innerText = `R$ ${Number(freePass).toFixed('2').replace('.',',')}`;
-    document.querySelector('#books').innerText = `R$ ${Number(books).toFixed('2').replace('.',',')}`;
-}
-setDom();
+const salary = localStorage.getItem('currentSalary');
+input.value = salary;
+const { requiredAccount, investment, freePass, books } = slice();
+setDomValues(salary,requiredAccount,investment,freePass,books);
